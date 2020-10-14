c1f8d3c11d00c14b0284954d4272906f5497cda7
tangwentao <15884437646@163.com>
2020-10-14 23:14:40 +0800

    add Netty Demo

---
+++ chat-center/src/main/java/com/example/chat/testForNIO/netty/NettyServer.java
@@ -0,0 +1,37 @@
+package com.example.chat.testForNIO.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+
+    public static void main(String[] args) throws Exception{
+
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boosGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
+            ChannelFuture sync = serverBootstrap.bind(6666).sync();
+
+            ChannelFuture sync1 = sync.channel().closeFuture().sync();
+        }finally {
+            boosGroup.shutdownGracefully();
+        }
+    }
+}
